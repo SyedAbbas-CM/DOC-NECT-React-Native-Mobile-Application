3062a6e73b6f716209cb2fcbeb940112450d5e6c
RaahimSiddiqi <k200146@nuedu.pk>
2022-11-20 23:58:29 +0500

    Created empty profile screen

---
+++ myproj/src/screens/ProfileScreen/ProfileScreen.js
@@ -0,0 +1,28 @@
+import { View, Text ,Image,StyleSheet, ImageBackground} from 'react-native'
+import React ,{useState} from 'react'
+
+
+const ProfileScreen = () => {
+    return ( 
+        <View style={styles.root}>
+            
+        </View>
+     );
+}
+ 
+
+const styles = StyleSheet.create({
+    root : {
+      flex:1,
+    },
+    input : {
+      marginVertical:10, 
+      backgroundColor : "white",
+      color : 'gray'
+    },
+    button : {
+      marginVertical:10,
+      borderRadius: 5
+    }
+  })
+  
