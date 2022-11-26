a1449ec14a904fe9e37eb5c927b3c400a7888a14
RaahimSiddiqi <k200146@nu.edu.pk>
2022-11-26 16:02:05 +0500

    forgot to push sql

--- Server/DB/create_db.sql
+++ Server/DB/create_db.sql
@@ -1,8 +1,7 @@
 use db;
 
 CREATE TABLE User(
-	userId    INTEGER AUTO_INCREMENT,
-    username  VARCHAR(255) NOT NULL UNIQUE,
+    userName  VARCHAR(255) NOT NULL UNIQUE,
     pass      VARCHAR(255) NOT NULL,
 	userRole  enum('USER', 'DOCTOR') NOT NULL,
     firstName VARCHAR(255),
@@ -13,59 +12,60 @@ CREATE TABLE User(
     gender    enum('MALE', 'FEMALE', 'OTHER'),
     about     TEXT,
     joinDate date default ( curdate() ),
-    primary key (userId)
+    primary key (userName)
 );
 
 CREATE TABLE Doctor(
-	doctorId  INTEGER,
+	docUserName  INTEGER,
     docPoints INTEGER default 0,
-    FOREIGN KEY(doctorId) REFERENCES User(userId)
+    FOREIGN KEY(userName) REFERENCES User(userName)
 );
 
 CREATE TABLE Certification(
-    doctorId 	   INTEGER,
+    docUserName    INTEGER,
     instituteName  VARCHAR(255),
 	degreeTitle    VARCHAR(255),
     startDate 	   DATE,
     endDate 	   DATE,
     approvalStatus bool,
-    PRIMARY KEY(doctorId),
-    FOREIGN KEY(doctorId) REFERENCES Doctor(doctorId)
+    PRIMARY KEY(docUserName),
+    FOREIGN KEY(docUserName) REFERENCES Doctor(docUserName)
 );
 
 CREATE TABLE History(
 	recordId 	   INTEGER AUTO_INCREMENT,
-    userId 		   INTEGER,
+    userName	   VARCHAR(255),
     ailmentName    VARCHAR(255),
-    symptoms 	   varchar(255),
+    description    TEXT,
+    symptoms	   TEXT,
     startDate	   DATE,
     endDate 	   DATE,
 	PRIMARY KEY(recordId),
-    FOREIGN KEY(userId) REFERENCES User(userId)
+    FOREIGN KEY(userName) REFERENCES User(userName)
 );
 
 CREATE TABLE Post(
 	postId 		 INTEGER AUTO_INCREMENT,
-    userId 		 INTEGER,
+    userName     INTEGER,
     title  		 VARCHAR(255) NOT NULL,
     body 		 TEXT,
     creationTime timestamp default ( current_timestamp() ),
     PRIMARY KEY(postId),
-    FOREIGN KEY(userId) REFERENCES User(userId)
+    FOREIGN KEY(userName) REFERENCES User(userName)
 );
 
 CREATE TABLE Comment(
 	commentId    	INTEGER AUTO_INCREMENT,
     parentCommentId INTEGER,
     postId       	INTEGER,
-	userId 	     	INTEGER,
+	userName 	     	INTEGER,
 	body 	     	TEXT,
     upvotes   	 	INTEGER default 0,
     creationTime 	timestamp,
     PRIMARY KEY(commentId),
 	FOREIGN KEY(parentCommentId) REFERENCES Comment(commentId),
     FOREIGN KEY(postId) REFERENCES Post(postId),
-    FOREIGN KEY(userId) REFERENCES User(userId)    
+    FOREIGN KEY(userName) REFERENCES User(userName)    
 );
 
 show tables;
@@ -87,4 +87,4 @@ desc Comment;
 select * from User;
 select * from Doctor;
 select * from Certification;
-select * from User U, UserProfile Up, Doctor D, Certification C where U.userId = Up.userId and U.userId = D.doctorId and D.doctorId = C.doctorId;
+select * from User U, UserProfile Up, Doctor D, Certification C where U.userName = Up.userName and U.userName = D.docUserName and D.docUserName = C.docUserName;
