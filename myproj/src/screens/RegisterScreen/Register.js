73a747eaabc56294f8e115b0295b62646d85dc70
SyedAbbas-CM <syed.abbas.ons@gmail.com>
2022-11-27 22:21:37 +0500

    Usecases for registration and signing in complete

--- myproj/src/screens/RegisterScreen/Register.js
+++ myproj/src/screens/RegisterScreen/Register.js
@@ -11,18 +11,48 @@ const Register = () => {
   const navigation = useNavigation();
   const [choice, setchoice] = useState(null);
   const [showCertifForm, setshowCertifForm] = useState(false);
+
   const onRegisterUser = (formData) => {
     /* Post it? */
     // axios.post().then(
       if(choice === "doctor") setshowCertifForm(true);
       else 
-        navigation.navigate('HomeScreen');
-    // )
+    axios.post("http://192.168.1.38:8090/api/register",{
+      uname:formData.userName,   
+      fname:formData.firstName,
+      role:"user",
+      lname:formData.lastName,
+      email:formData.email,
+      dob:formData.dob,
+      pass:formData.password
+      }).then((response)=>{
+      console.log(response);
+      if(response.status === 200)
+      navigation.navigate('Signin');
+      else{
+        console.warn(response)
+      }
+      }
+      ).catch(function (error){
+      if (error.response) {
+
+      //console.log(error.response.data);
+       //console.log("Error: "+error.response.status);
+      //console.log(error.response.headers);
+
+      }else if (error.request) {
+      //console.log("No Response: "+error.request);
+      }else {
+        //console.log('Error sending request!', error.message);
+      }
+});
   }
   const onSubmitCertificationForm = (formData) => {
-    // axios.post().then(
-      navigation.navigate('HomeScreen');
-    // )
+
+    
+
+
+
   }
   return (
     choice === null ?
